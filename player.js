cbd9868a5b57b588483ea32b0c754dfe5ceb6c83
raghavbhaiji <rg378875@gmail.com>
2024-05-12 20:19:54 +0530

    Update player.js

--- player.js
+++ player.js
@@ -24,11 +24,11 @@ const { queueNames } = require("./commands/play");
 
 function initializePlayer(client) {
     const nodes = [
-         {
-    host: "lavalink.oryzen.xyz",
-    port: 80, 
-    password: "oryzen.xyz", 
-     secure: false
+    {
+            host: "lava-v4.ajieblogs.eu.org",
+            password: "https://dsc.gg/ajidevserver",
+            port: 443,
+            secure: true
         },
     ];
 
